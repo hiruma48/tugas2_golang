99558cf7d72969ca52b15af1289298fbe712890c
hiruma48 <49358000+hiruma48@users.noreply.github.com>
2023-11-09 15:19:12 +0700

    tugas 2 golang
    done

---
+++ tugas 2/tugas2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var angka = 6
+
+	if angka%2 != 0 {
+
+		fmt.Printf("%d ganjil\n", angka)
+
+	} else {
+
+		fmt.Printf("%d genap\n", angka)
+
+	}
+
+}
